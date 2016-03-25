7b56790779c22eae4b42ea2aaf9b4a5c3434fcff
gauravjos <gauravjos@cybage.com>
2016-03-25 12:49:11 +0000

    Added docs and edited php and mysql app containers

--- rkt_aci_images/build-apache_php.sh
+++ rkt_aci_images/build-apache_php.sh
@@ -4,16 +4,16 @@ acbuild --debug begin
 
 acbuild --debug set-name example.com/apache2_php
 
-acbuild --debug dependency add quay.io/appcontainers/ubuntu
+acbuild --debug dependency add quay.io/appcontainers/ubuntu:latest
 acbuild --debug set-user root 
 
 acbuild --debug port add dns udp 53
-acbuild --debug run -- chmod 755 /
 acbuild --debug run -- apt-get update
-acbuild --debug run -- apt-get -y install apache2 apache2-utils php5-common php5-mysql php5-gd
+acbuild --debug run -- apt-get -y install apache2 apache2-utils php5-common php5-mysql php5-gd php-apc php-pear php5-apcu php5-cli php5-common php5-curl php5-gd php5-json php5-mysql php5-readline libapache2-mod-php5
+
 acbuild --debug copy /root/app /app
 acbuild --debug copy /root/run.sh /run.sh
-
+acbuild --debug run -- /bin/chmod +x /run.sh
 acbuild --debug port add http tcp 80
 
 acbuild --debug set-exec -- /run.sh ;
--- rkt_aci_images/build-mysqldb.sh
+++ rkt_aci_images/build-mysqldb.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 
 acbuild --debug begin
 
@@ -6,15 +7,16 @@ acbuild --debug set-name example.com/mysql
 
 acbuild --debug dep add quay.io/appcontainers/ubuntu:latest
 acbuild --debug set-user root 
-
-acbuild --debug port add dns udp 53
-#acbuild --debug run -- chmod 755 /
+acbuild --debug run -- 'chmod 755 /'
 acbuild --debug run -- apt-get update
 acbuild --debug run -- apt-get -y install mysql-server-5.6
+acbuild --debug environment add DEBIAN_FRONTEND noninteractive
 acbuild --debug copy /root/mysql/my.cnf /etc/mysql/conf.d/my.cnf
 acbuild --debug copy /root/mysql/br.sql br.sql
 acbuild --debug copy /root/mysql/run.sh /usr/local/bin/run
-acbuild --debuf run -- chmod 755 /usr/local/bin/run
+acbuild --debug run -- /bin/chmod +x /usr/local/bin/run
+
+acbuild --debug run -- /bin/chmod 755 /usr/local/bin/run
 acbuild --debug port add mysql tcp 3306
 acbuild --debug mount add mysql /var/lib/mysql
 acbuild --debug set-exec -- /usr/local/bin/run ;
